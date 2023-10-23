aaf17793bd0b7a21f8f1f37523ef2211a4a2a630
bipentihexium <bipentihexium@gmail.com>
2023-10-23 19:42:36 +0200

    Add annotation popup urls

--- fe/fhtml/harmonogram.js
+++ fe/fhtml/harmonogram.js
@@ -1,12 +1,15 @@
 const api="https://api.simp.klubkepler.eu/";
 const days=["Čtvrtek 16.11.","Pátek 17.11.","Sobota 18.11"];
 const dayids=["streda/","ctvrtek/","patek/"];
+var tt_lkp={};
+var tt_ld=false;
 function format_update(utct){
 	let d=new Date(utct*1000);
 	return d.getDate()+"."+(d.getMonth()+1)+". "+("00"+d.getHours()).slice(-2)+":"+("00"+d.getMinutes()).slice(-2)+":"+("00"+d.getSeconds()).slice(-2)
 }
 function lecture_popup(lec,title,time,room,id){
 	return async function(){
+		console.log("fetching "+api+"anotace/"+id);
 		const resp=await fetch(api+"anotace/"+id);
 		const data=await resp.json();
 		document.getElementById("lecture_popup").style.display="block";
@@ -17,16 +20,27 @@ function lecture_popup(lec,title,time,room,id){
 		document.getElementById("ov_annotation").textContent=data.data.info.annotation;
 		document.getElementById("ov_lecturer_info").textContent=data.data.info.lecturer_info;
 		document.getElementById("ov_last_update").textContent="data z "+format_update(data.data.last_updated);
+		window.history.pushState("","","/anotace/"+id);
+		window.onpopstate=function(e){hide_lecture();};
 	};
+}
+function popup(a){
+	if(!tt_ld)
+		setTimeout(function(){popup(a);},100);
+	else if(a in tt_lkp)
+		tt_lkp[a]();
 }
 function hide_lecture(){
 	document.getElementById("lecture_popup").style.display="none";
+	window.history.pushState("","","/");
+	window.onpopstate=null;
 }
 function make_table(div,data,day,dayid){
 	let tt=div.appendChild(document.createElement("h4"));
 	tt.classList.add("day_title");
 	tt.appendChild(document.createTextNode(day));
 	const table=div.appendChild(document.createElement("table"));
+	table.id="timetable_"+dayid;
 	for(let i=0;i<data.length;i++){
 		const dr=data[i];
 		const tr=table.insertRow();
@@ -59,6 +73,7 @@ function make_table(div,data,day,dayid){
 				if(dd.id!=null){
 					td.onclick=lecture_popup(dd.title,dd.lecturer,tm,data[0][j]===null?"":data[0][j].title,dayid+dd.id);
 					td.classList.add("clickable");
+					tt_lkp[dayid+dd.id]=td.onclick;
 				}
 			}
 		}
@@ -69,6 +84,7 @@ async function gen_tables(){
 	while(tables_div.firstChild){
 		tables_div.removeChild(tables_div.lastChild);
 	}
+	console.log("fetching "+api+"harmonogram");
 	const resp=await fetch(api+"harmonogram");
 	const data=await resp.json();
 	const hd=data.data.harmonogram;
@@ -78,5 +94,6 @@ async function gen_tables(){
 	let tu=tables_div.appendChild(document.createElement("span"));
 	tu.classList.add("last_update");
 	tu.textContent="data z "+format_update(data.data.last_updated);
+	tt_ld=true;
 }
 gen_tables();
