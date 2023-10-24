23a9aff01b2c05d581cb513082c38437dde822ed
Pandicon <70060103+Pandicon@users.noreply.github.com>
2023-10-24 14:28:59 +0200

    Add rowspan and colspan to the TableCell, make id optional

--- gjk_symposion_2023_api/src/main/java/com/pandicon/gjk_symposion_2023_api/api_model/TableCell.java
+++ gjk_symposion_2023_api/src/main/java/com/pandicon/gjk_symposion_2023_api/api_model/TableCell.java
@@ -1,15 +1,21 @@
 package com.pandicon.gjk_symposion_2023_api.api_model;
 
+import java.util.Optional;
+
 public class TableCell {
     public String lecturer;
     public String title;
     public boolean for_younger;
-    public String id;
+    public Optional<String> id;
+    public int rowspan;
+    public int colspan;
 
-    public TableCell(String i_lecturer, String i_title, boolean i_for_younger, String i_id) {
+    public TableCell(String i_lecturer, String i_title, boolean i_for_younger, Optional<String> i_id, int i_rowspan, int i_colspan) {
         this.lecturer = i_lecturer;
         this.title = i_title;
         this.for_younger = i_for_younger;
         this.id = i_id;
+        this.rowspan = i_rowspan;
+        this.colspan = i_colspan;
     }
 }
