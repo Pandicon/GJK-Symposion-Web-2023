d39ff22e686cd28495ac9eb7920649f01c68d8d3
Ozone <70636940+LukyGithub@users.noreply.github.com>
2023-10-29 21:45:53 +0100

    Optimazation of the navbar for smaller devices
    the calculation of the resolution of the webgl canvas renderer still needs some work. RN it might be to heavy for mobile compute chips.

--- fe/fhtml/main.js
+++ fe/fhtml/main.js
@@ -109,7 +109,8 @@ if (!gl) {
 	document.onmousedown=function(){tms=1.3;tmh=12.0;};
 	document.onmouseup=function(){tms=0.3;tmh=6.0;};
 	function render(){
-		canvas.width = document.body.clientWidth;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerWidth>1100? 100 : 200;
 		const time=new Date().getTime()-begin_t;
 		gl.viewport(0,0,canvas.width,canvas.height);
 		ms=(tms+ms)*0.5;
